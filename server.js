3c65c13791ab031228eed8d75f0108f8b656d360
Mehedi Hassan Shifat <34704511+mhshifat@users.noreply.github.com>
2018-10-25 13:19:26 +0600

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 import express from "express";
 import color from "colors";
 import morgan from "morgan";
-import cors from "cors";
 import helmet from "helmet";
 import bodyParser from "body-parser";
 
@@ -16,7 +15,6 @@ import { port } from "./config/config";
 const app = express();
 
 // Use Middlewares
-app.use(cors());
 app.use(helmet());
 app.use(morgan("tiny"));
 app.use(bodyParser.json());
